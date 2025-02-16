e3e260a76888c5b2822a06b8478c639b0bfbabdf
Airesh Bhat <aireshbhat@Aireshs-Mac-mini.local>
2025-02-16 09:14:07 +0530

    Add iroh-willow base protocol and storage from iroh-willow repository

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod proto;
+mod store;
+mod form;
+mod interest;
+mod util;
+mod net;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/proto/mod.rs
@@ -0,0 +1,6 @@
+pub mod keys;
+pub mod data_model;
+pub mod meadowcap;
+pub mod pai;
+pub mod grouping;
+pub mod wgps;
